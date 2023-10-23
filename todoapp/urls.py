50350df9b17fa91c2f31510d55ded8b4e1d7d53f
shamimhcp1 <shamimhcp1@gmail.com>
2023-10-23 18:29:22 +0600

    urls & views updated

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -3,9 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('add_todo', views.add_todo, name='add_todo'),
     path('edit_todo/<int:todo_id>/', views.edit_todo, name='edit_todo'),
-    path('update_todo/<int:todo_id>/', views.update_todo, name='update_todo'),
     path('delete_todo/<int:todo_id>/', views.delete_todo, name='delete_todo'),
     path('complete_todo/<int:todo_id>/', views.complete_todo, name='complete_todo')
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -4,20 +4,15 @@ from . models import Todo
 
 # Create your views here.
 def index(request):
+    # Add Todo if request is post
+    if request.method == 'POST':
+        todo = Todo(content = request.POST['content'])
+        todo.save()
+        return redirect('/todo/')
+
     context = {'todo_list' : Todo.objects.all().order_by('-id').values()}
     return render(request, 'todoapp/todo_list.html', context)
 
-def add_todo(request:HttpRequest):
-    todo = Todo(content = request.POST['content'])
-    todo.save()
-    return redirect('/todo/')
-
-
-def update_todo(request, todo_id):
-    todo = Todo.objects.get(pk=todo_id)
-    todo.content = request.POST['content']
-    todo.save()
-    return redirect('/todo/')
 
 def complete_todo(request, todo_id):
     todo = Todo.objects.get(pk=todo_id)
@@ -35,5 +30,13 @@ def delete_todo(request, todo_id):
     return redirect('/todo/')
 
 def edit_todo(request, todo_id):
+
+    # Update Todo content if request is post
+    if request.method == 'POST':
+        todo = Todo.objects.get(pk=todo_id)
+        todo.content = request.POST['content']
+        todo.save()
+        return redirect('/todo/')
+    
     todo = Todo.objects.get(pk=todo_id)
     return render(request, 'todoapp/todo_edit.html', {'todo': todo})
